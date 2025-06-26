108e12301e630ee1947a748925668b29a27001d9
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2025-06-26 20:46:45 +0530

    Timetable Issue

--- src/components/layout/TopBar.jsx
+++ src/components/layout/TopBar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Menu, LogOut } from 'lucide-react'
-import { useLocation, matchPath } from 'react-router-dom';
+import { useLocation, matchPath, useNavigate } from 'react-router-dom';
 
 function Topbar({ onClick }) {
+
     const location = useLocation();
+    const navigate = useNavigate();
 
     const menuNames = {
         '/layout/:staffId/home': 'Home',
@@ -26,7 +28,7 @@ function Topbar({ onClick }) {
         <div className='py-2 px-3 flex flex-row items-center justify-between bg-gradient-to-r from-blue-500 via-blue-500 to-blue-600'>
             <button onClick={onClick} className='p-1 rounded hover:bg-white/20 transition'><Menu color='white' size={24} /></button>
             <label className='text-white font-semibold text-lg '>{currentMenu}</label>
-            <button className='p-1.5 rounded hover:bg-white/20 transition'><LogOut color='white' size={22} /></button>
+            <button onClick={() => navigate('/')} className='p-1.5 rounded hover:bg-white/20 transition'><LogOut color='white' size={22} /></button>
         </div>
     )
 }
