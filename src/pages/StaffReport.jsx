0b748cd52b9be03f960335398081fd41304f0518
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2025-08-11 18:19:41 +0530

    Report

--- src/pages/StaffReport.jsx
+++ src/pages/StaffReport.jsx
@@ -17,7 +17,7 @@ function StaffReport() {
             <div className='flex justify-end'>
                 <button
                     onClick={getStaffReport}
-                    className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
+                    className="bg-blue-600 w-40 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
                 >
                     Get Staff Report
                 </button>
@@ -39,10 +39,16 @@ function StaffReport() {
 
             {/* Table */}
             {!loading && !error && data?.length > 0 && (
-                <div className="overflow-x-auto">
+                <div className="overflow-x-auto flex flex-col items-end">
+                    <button
+                        className="mb-6 w-40 bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 transition"
+                    >
+                        Download Excel
+                    </button>
                     <table className="text-center w-full bg-white rounded shadow-md border border-gray-300 border-collapse">
                         <thead className="bg-blue-500 text-white">
                             <tr>
+                                <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">S No</th>
                                 <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">Staff ID</th>
                                 <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">Full Name</th>
                                 <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">Attendance Count</th>
@@ -51,6 +57,9 @@ function StaffReport() {
                         <tbody>
                             {data.map((staff, index) => (
                                 <tr key={index} className="border">
+                                    <td className="px-2 py-2 border border-gray-300 whitespace-nowrap">
+                                        {index+1}
+                                    </td>
                                     <td className="px-2 py-2 border border-gray-300 whitespace-nowrap">
                                         {staff.staffId}
                                     </td>
@@ -71,7 +80,7 @@ function StaffReport() {
             {!loading && !error && data?.length === 0 && (
                 <div className="text-center text-gray-500 py-4">No staff report available.</div>
             )}
-            
+
         </div>
     )
 }
--- src/pages/StudentReport.jsx
+++ src/pages/StudentReport.jsx
@@ -8,14 +8,14 @@ function ClassReport() {
     const apiUrl = import.meta.env.VITE_API_URL;
     const { loading, error, data, fetchData } = useFetch();
 
-    const getStudentReport = () => { fetchData(`${apiUrl}/api/report/student`)};
+    const getStudentReport = () => { fetchData(`${apiUrl}/api/report/student`) };
 
     return (
         <div className='p-6 w-full space-y-6'>
             <div className='flex justify-end'>
                 <button
                     onClick={getStudentReport}
-                    className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
+                    className="bg-blue-600 w-48 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
                 >
                     Get Student Report
                 </button>
@@ -37,12 +37,19 @@ function ClassReport() {
 
             {/* REPORT TABLE */}
             {!loading && !error && data?.length > 0 && (
-                <div className="overflow-x-auto">
+                <div className="overflow-x-auto flex flex-col items-end">
+                    <button
+                        className="mb-6 w-48 bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 transition"
+                    >
+                        Download Excel
+                    </button>
                     <table className="text-center w-full bg-white rounded shadow-md border border-gray-300 border-collapse">
                         <thead className="bg-blue-500 text-white">
                             <tr>
+                                <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">S No</th>
                                 <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">Reg No</th>
                                 <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">Roll No</th>
+                                <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">Year</th>
                                 <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">Total Classes</th>
                                 <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">Present</th>
                                 <th className="px-4 py-2 border border-gray-300 whitespace-nowrap">Absent</th>
@@ -52,10 +59,16 @@ function ClassReport() {
                             {data.map((student, index) => (
                                 <tr key={index} className="border">
                                     <td className="px-2 py-2 border border-gray-300 whitespace-nowrap">
+                                        {index+1}
+                                    </td>
+                                    <td className="px-2 py-2 border border-gray-300 whitespace-nowrap">
                                         {student.reg_no}
                                     </td>
                                     <td className="px-2 py-2 border border-gray-300 whitespace-nowrap">
                                         {student.roll_no}
+                                    </td>
+                                    <td className="px-2 py-2 border border-gray-300 whitespace-nowrap">
+                                        {student.year}
                                     </td>
                                     <td className="px-2 py-2 border border-gray-300 whitespace-nowrap">
                                         {student.total}
